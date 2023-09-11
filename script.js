6c9b33d52ffef475dbeedeb58876bc38f80e9553
Vincent Kiathadi <vincentkiathadi@gmail.com>
2023-09-12 00:46:04 +0800

    banner bestseller

--- script.js
+++ script.js
@@ -15,3 +15,36 @@ mobileMenuX.addEventListener("click",mobileMenuIn);
 function mobileMenuIn(){
     mobileMenu.classList.add("noneMenu");
 }
+
+
+var bestsells=[
+    {
+        name:"pink shirt",
+        price:"$25",
+        size:"large"
+    },
+    {
+        name:"blue shirt",
+        price:"$15",
+        size:"small"
+    },
+    {
+        name:"orange shirt",
+        price:"$23",
+        size:"medium"
+    }
+]
+
+var bestSellGallery=document.getElementById("bestSellerGallery");
+
+bestsells.forEach(getShown=>{
+    var theSell=document.createElement("button");
+    theSell.innerHTML=`<p>Name is ${getShown.name}</p>
+                        <br>
+                        <p>Price is ${getShown.price}</p>`
+
+    bestSellGallery.appendChild(theSell);
+    
+    
+    
+});
